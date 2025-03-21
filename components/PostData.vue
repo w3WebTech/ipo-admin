78f901ba6eac32078724df89e2be4e0b015ae8b0
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-03-21 11:57:31 +0530

    Update PostData.vue

--- components/PostData.vue
+++ components/PostData.vue
@@ -248,7 +248,7 @@ const submitData = async () => {
 const getBase64 = (file) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
-        reader.readAsData URL(file);
+        reader.readAsDataURL(file);
         reader.onload = () => resolve(reader.result);
         reader.onerror = (error) => reject(error);
     });
